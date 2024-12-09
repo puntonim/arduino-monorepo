9de94cc5aa42ef084e815da22d0e8a9ae262b1eb
puntonim <puntonim@gmail.com>
2024-12-09 21:53:31 +0100

    feat(multiple-push-buttons-with-ioabstraction): new example

--- DEVICE__PUSH_BUTTON_(MOMENTARY_SWITCH)/multiple-push-buttons-with-ioabstraction/src/multiple-push-buttons-with-ioabstraction.cpp
+++ DEVICE__PUSH_BUTTON_(MOMENTARY_SWITCH)/multiple-push-buttons-with-ioabstraction/src/multiple-push-buttons-with-ioabstraction.cpp
@@ -1,18 +1,17 @@
 /*
- * PUSH BUTTON TOGGLE WITH IOABSTRACTION
- * =====================================
- * Detect the push of a button and toggle the state of the bultin LED
- *  using the lib IOAbstraction. Also, detect when the button is held down for
- *  >2 secs.
+ * MULTIPLE PUSH BUTTONS WITH IOABSTRACTION
+ * ========================================
+ * Detect pushes of any of the 3 buttons using the lib IOAbstraction.
+ * Also, detect when the button is held down for >2 secs.
  *
  * Note: push buttons are momentary switches.
  * Note: button connected with the integrated pull-up resistor.
  * Note: Arduino UNO R4 seems to have only 2 PINs that support interrupts
  *  pin 2 and 3, see:
  *  https://docs.arduino.cc/language-reference/en/functions/external-interrupts/attachInterrupt/
- *  however this code seems to work even on pin 6 and I am sure that no tasks
- *  is scheduled to execute the polling (because I debugged it with
- *  printDebugTasks()).
+ *  however I proved that it works also on other PINs (like 6).
+ *  BUT IT DOESN'T WORK WITH 3 BUTTONS!!
+ *  SO, WITH 3 BUTTONS, YOU MUST USE POLLING.
  *
  * PROS
  * ----
@@ -24,7 +23,7 @@
  * - None.
  *
  * Devices:
- *  1x push button (using Arduino's internal pull-up resistor)
+ *  3x push button (using Arduino's internal pull-up resistor)
  *
  * Docs:
  *  - https://tcmenu.github.io/documentation/arduino-libraries/io-abstraction/
@@ -42,36 +41,36 @@
 // Functions declaration.
 void onButtonTriggered(uint8_t pin, bool isHeldDown);
 
-const int PUSH_BUTTON_PIN = 6;
-bool isLedOn = false;
+const unsigned short BUTTON_A_PIN = 2;
+const unsigned short BUTTON_B_PIN = 3;
+const unsigned short BUTTON_C_PIN = 4;
 
 void setup() {
   Serial.begin(57600);
   Serial.println("START");
 
-  // Declare the integrated LED on pin 13 as output.
-  pinMode(LED_BUILTIN, OUTPUT);
-
   // Options: SWITCHES_NO_POLLING, SWITCHES_POLL_EVERYTHING,
   // SWITCHES_POLL_KEYS_ONLY (rotary encoders by interrupts, buttons by poll).
   // Last arg (true) is for integrated pull-up resistors.
-  switches.init(ioUsingArduino(), SWITCHES_NO_POLLING, true);
+  // IMP: SINCE WE ARE USING 3 BUTTONS THEN WE HAVE TO USE POLLING (instead of
+  //  interrupts because the limit in Arduino UNO R4 is 2 interrupts).
+  switches.init(ioUsingArduino(), SWITCHES_POLL_KEYS_ONLY, true);
+
   // If using a pull-down resistor instead, then use the arg `invertLogic =
   // true` (last arg).
   // switches.addSwitch(PUSH_BUTTON_PIN, onButtonTriggered, NO_REPEAT, true);
-  switches.addSwitch(PUSH_BUTTON_PIN, onButtonTriggered, NO_REPEAT);
+  switches.addSwitch(BUTTON_A_PIN, onButtonTriggered, NO_REPEAT);
+  switches.addSwitch(BUTTON_B_PIN, onButtonTriggered, NO_REPEAT);
+  switches.addSwitch(BUTTON_C_PIN, onButtonTriggered, NO_REPEAT);
 }
 
 void onButtonTriggered(uint8_t pin, bool isHeldDown) {
   if (isHeldDown) {
-    // Hold down the button for 2 secs.
-    Serial.println("HELD FOR 2 SECS");
+    Serial.println((String) "BUTTON " + pin + " HELD FOR 2 SECS");
     return;
   }
 
-  Serial.println("PRESSED");
-  isLedOn = !isLedOn;
-  digitalWrite(LED_BUILTIN, isLedOn);
+  Serial.println((String) "BUTTON " + pin + " PRESSED");
 }
 
 void loop() { taskManager.runLoop(); }
